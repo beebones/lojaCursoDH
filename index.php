973610ff50e393d2b93e78c02786e23b5dbd57fe
beebones <gelbundschwarz@gmail.com>
2019-10-02 12:05:01 -0300

    Adicionando estrutura de produtos e categotias dinâmicas

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
     $nomeSistema = "Cursos da Bee";
     $usuario = ["nome"=>"BeeBones"];
+    $produtos = [
+        ["nome"=>"Curso Javascript", "preco"=>"1.200.00", "duracao"=>"5 meses", "img"=>"img/js.png"],
+        ["nome"=>"Curso HTML", "preco"=>"400.00", "duracao"=>"3 meses", "img"=>"img/html.png"],
+    ];
+    $categorias = ["Cursos", "Palestras", "Artigos"];
 ?>
 
 <!DOCTYPE html>
@@ -13,51 +18,54 @@
     <title>Document</title>
 </head>
 <body>
-    <header class="navbar">
-        <h1 id="logo"><?php echo $nomeSistema; ?></h1>
-        <nav>
+    <header>
+        <div class="navbar">
+            <h1 id="logo"><?php echo $nomeSistema; ?></h1>
+            <nav>
+                <ul class="nav">
+                <?php if(isset($usuario) && $usuario != "") {?>            
+                    <li class="nav-item"><a href="#" class="nav-link">Cursos</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Olá <?php echo $usuario["nome"]; ?></a></li>                
+                <?php } else { ?>
+                    <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Cadastrar</a></li>
+                <?php } ?>
+                </ul>
+            </nav>
+        </div>
+        <!-- categorias -->
+        <nav class="navbar bg-dark">
             <ul class="nav">
-            <?php if(isset($usuario) && $usuario != "") {?>            
-                <li class="nav-item"><a href="#" class="nav-link">Cursos</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Olá <?php echo $usuario["nome"]; ?></a></li>                
-            <?php } else { ?>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Cadastrar</a></li>
-            <?php } ?>
+                <?php if(isset($categorias) && $categorias != []){ ?>
+                    <?php foreach($categorias as $categoria){ ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#"><?php echo $categoria; ?></a>
+                        </li>
+                    <?php } ?>
+                <?php } ?>
             </ul>
         </nav>
     </header>
+        
     <main>
         <section class="container mt-4">
             <div class="row justify-content-around">
-                <div class="col-lg-3 card text-center">               
-                    <h2>Curso HTML</h2>
-                    <img src="img/html.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">R$15,00</h5>
-                        <a href="#" class="btn btn-primary">Buy</a>
+            <?php if(isset($produtos) && $produtos != []){ ?>
+                <?php foreach($produtos as $produto){ ?>
+                    <div class="col-lg-3 card text-center">               
+                        <h2><?php echo $produto['nome']; ?></h2>
+                        <img src="<?php echo $produto['img']; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $produto['preco']; ?></h5>
+                            <a href="#" class="btn btn-primary">Buy</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-3 card text-center">               
-                    <h2>Curso CSS</h2>
-                    <img src="img/css.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">R$35,00</h5>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 card text-center">               
-                    <h2>Curso JavaScript</h2>
-                    <img src="img/js.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">R$85,00</h5>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
-
-
-
-                </div>
+                <!-- Fechando PHP do foreach -->
+                <?php } ?>               
+            <!-- fechando o if -->
+            <?php } else { ?>
+                <h1>Não tem produtos cadastrados nessa sessão :(</h1>
+            <?php } ?>
             </div>
         </section>
     </main>
